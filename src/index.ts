b3460b312d9c5dd889db55d3b9d62af133db17ff
Oralita <54722120+oat431@users.noreply.github.com>
2024-11-08 19:55:38 +0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import express, { urlencoded } from 'express';
+import swaggerUI from 'swagger-ui-express';
+import apiDocs from './config/swagger.config.js';
+
+import healthCheckRouter from './module/HealthCheck/router/health-check.router.js';
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(urlencoded({ extended: true }));
+
+app.use('/api/v1', healthCheckRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+// swagger setup
+app.use('/swagger', swaggerUI.serve, swaggerUI.setup(apiDocs));
+
+app.get('/api-docs', (req, res) => {
+    res.json(apiDocs);
+});
+// end swagger setup
+
+app.listen(PORT, () => {
+    console.info(`Server is running on : http://localhost:${PORT}`);
+});
