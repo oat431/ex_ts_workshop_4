b261624c9ca4712144594f7771c5c49c0f20300b
oat431 <oat431@gmail.com>
2024-11-09 01:32:19 +0700

    feat: add logger

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { urlencoded, json } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import apiDocs from './config/swagger.config.js';
+import morgan from 'morgan';
 
 import healthCheckRouter from './module/HealthCheck/router/health-check.router.js';
 import movieRouter from './module/MovieManagement/router/movie.router.js';
@@ -10,6 +11,7 @@ const app = express();
 
 app.use(json());
 app.use(urlencoded({ extended: true }));
+app.use(morgan(':date[iso] | :method | :url | :status | :res[content-length] - :response-time ms'));
 
 // feature
 app.use('/api/v1', healthCheckRouter);
